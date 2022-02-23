a7600b8261cb237ad7a81bb9c404d27bee0d5306
Thushan Fernando <thushan@thushanfernando.com>
2022-02-23 19:49:05 +1100

    Initial base packages & infrastructure for debian.

---
+++ linux-debian/install-base.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+
+source "$(dirname "${BASH_SOURCE[0]}")./scripts/console-colours.sh"
+
+if [[ $EUID -ne 0 ]]; then
+   show_error "Script needs to run elevated."
+   exit 1
+fi
+
+while getopts u:a:f: flag
+do
+    case "${flag}" in        
+        f) sourcefile=${OPTARG};;
+    esac
+done
+
+[[ -z "$sourcefile" ]] && die "-f [sourcefile] missing."
+[ ! -f "$sourcefile" ] && die "sourcefile '$sourcefile' does not exist."
+
+show_info "Updating sources..."
+apt update && apt upgrade || die 'Failed updating sources.'
+show_success "Updating sources...done!"
+
+show_info "Installing packages from '$sourcefile'..."
+xargs -a <(awk '! /^ *(#|$)/' "$sourcefile") -r -- apt install
+show_success "Installing packages from '$sourcefile'...Done!"
---
+++ scripts/console-colours.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+
+ERROR=`tput setaf 1`
+INFO=`tput setaf 4`
+SUCCESS=`tput setaf 2`
+BOLD=`tput smso`
+UNBOLD=`tput rmso`
+ENDMARKER=`tput sgr0`
+
+show_error() {
+    echo -e "${BOLD}${ERROR}ERROR:${UNBOLD} ${ERROR}$1 ${ENDMARKER}" 1>&2
+}
+
+show_info() {
+    echo -e "${INFO}$1 ${ENDMARKER}"    
+}
+
+show_success() {
+    echo -e "${SUCCESS}$1 ${ENDMARKER}"    
+}
+die() {
+     show_error "$1" 1>&2
+     exit 1
+}
