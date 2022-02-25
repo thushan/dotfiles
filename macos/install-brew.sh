095199eae61590f41bfada181cf8d7b7e5a97fc8
Thushan Fernando <thushan@thushanfernando.com>
2022-02-25 22:10:41 +1100

    Refactor brew packages for the moment to be in one file.'

--- macos/install-brew.sh
+++ macos/install-brew.sh
@@ -1,24 +1,75 @@
 #!/usr/bin/env bash
+BASE=(
+    # System
+    bash-completion
+    curl
+    git
+    htop
+    jq
+    pkg-config
+    smartmontools
+    ssh-copy-id
+    tmux
+    tree
+    unrar
+    unzip
+    wget
+    youtube-dl
+    zsh-syntax-highlighting
+
+    # Developer
+    awscli
+    go
+    hugo
+    rustup-init
+    tinygo
+)
+CASKS=(
+    # System
+    keepassxc
+    gpgtools
+    iterm2
+    flux
+
+    # Internet Tools / Browsers
+    brave-browser
+    dropbox
+    firefox
+    google-chrome
+    opera
+    signal
+    slack
+    zoom
+
+    # Utilities
+    balenaetcher
+
+    # Developer
+    dotnet
+    postman
+    visual-studio-code
+
+    # Media
+    vlc
+)
+FONTS=(
+    font-inconsolidata
+    font-roboto
+)
 
 source "$(dirname "${BASH_SOURCE[0]}")./scripts/console-colours.sh"
 
-while getopts f:c: flag
-do
-    case "${flag}" in
-        f) sourcefile=${OPTAsRG};;
-        c) caskfile=${OPTARG};;
-    esac
-done
-
 if test ! $(which brew); then
-    show_info "Installing homebrew..."    
+    show_info "Installing homebrew..."
     /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+    show_success "Installing homebrew...Done!"
 fi
 
 show_info "Updating brew packages..."
-# brew update && brew upgrade `brew outdated`  || die 'Failed update & upgrade brew packages.'
+brew update || die 'Failed update & upgrade brew packages.' # && brew upgrade `brew outdated` 
 show_success "Updating brew packages...done!"
 
+# Setup sensible GNU utilities
 show_info "Configuring GNU Core Utilities..."
 brew tap homebrew/dupes
 brew install bash
@@ -31,28 +82,32 @@ brew install gnu-tar --with-default-names
 brew install gnu-which --with-default-names
 show_success "Configuring GNU Core Utilities...Done!"
 
-if test ! -z "$sourcefile"
-then
-    [ ! -f "$sourcefile" ] && die "sourcefile '$sourcefile' does not exist."
+show_info "Installing BASE brew packages..."
+brew install ${BASE[@]}
+show_success "Installing BASE brew packages...Done!"
 
-    show_info "Installing brew packages from '$sourcefile'..."
-    xargs -a <(awk '! /^ *(#|$)/' "$sourcefile") -r -- brew install
-    show_success "Installing brew packages from '$sourcefile'...Done!"
-    show_info "Cleaning up brew..."
-    brew cleanup
-    show_success "Cleaning up brew...Done!"
-fi
+show_info "Cleaning up brew..."
+brew cleanup
+show_success "Cleaning up brew...Done!"
 
-if test ! -z "$caskfile"
-then
-    [ ! -f "$caskfile" ] && die "caskfile '$caskfile' does not exist."
-    show_info "Installing brew Cask..."
-    brew install caskroom/cask/brew-cask
-    show_success "Installing brew Cask...Done!"
-    show_info "Installing brew cask packages from '$caskfile'..."
-    xargs -a <(awk '! /^ *(#|$)/' "$caskfile") -r -- brew cask install
-    show_success "Installing brew cask packages from '$caskfile'...Done!"
-    show_info "Cleaning up brew..."
-    brew cleanup
-    show_success "Cleaning up brew...Done!"
-fi
+show_info "Installing brew Cask..."
+brew install caskroom/cask/brew-cask
+show_success "Installing brew Cask...Done!"
+
+show_info "Installing brew cask packages..."
+brew cask install ${CASKS[@]}
+show_success "Installing brew cask packages...Done!"
+
+show_info "Installing brew Fonts Cask..."
+brew tap caskroom/fonts
+show_success "Installing brew Fonts Cask...Done!"
+
+show_info "Installing brew fonts packages..."
+brew cask install ${FONTS[@]}
+show_success "Installing brew cask packages...Done!"
+
+show_info "Cleaning up brew..."
+brew cleanup
+show_success "Cleaning up brew...Done!"
+
+show_success "We're done brew! /NZ-accent"
