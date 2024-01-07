c72c9aac185880dcf6484514d592054a548a0436
Thushan Fernando <thushan@thushanfernando.com>
2024-01-08 10:46:24 +1100

    Remove power settings for Apple Silicon and leave as default.

--- macos/default.sh
+++ macos/default.sh
@@ -12,38 +12,6 @@ show_info "Initialising..."
 sudo -v
 show_info "Got Sudo and you know it..."
 
-## POWER
-show_info "POWER SETTINGS..."
-
-# Hibernation mode
-# 0: Disable hibernation (speeds up entering sleep mode)
-# 3: Copy RAM to disk so the system state can still be restored in case of a
-#    power failure.
-sudo pmset -a hibernatemode 0
-
-# Remove the sleep image file to save disk space
-sudo rm /private/var/vm/sleepimage
-
-# Create a zero-byte file instead…
-sudo touch /private/var/vm/sleepimage
-# …and make sure it can’t be rewritten
-sudo chflags uchg /private/var/vm/sleepimage
-
-# Enable lid wakeup
-sudo pmset -a lidwake 1
-
-# Disable machine sleep while charging
-sudo pmset -c sleep 0
-
-# Set machine sleep to 5 minutes on battery
-sudo pmset -b sleep 5
-
-# Sleep the display after 15 minutes
-sudo pmset -a displaysleep 15
-
-# Set standby delay to 24 hours (default is 1 hour)
-sudo pmset -a standbydelay 86400
-
 ## SYSTEM
 show_info "SYSTEM SETTINGS..."
 
